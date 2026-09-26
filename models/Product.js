db3bf8c655b443b9a6916bb8b0de236b3f78e2c6
multicoder599 <newtonmulti@gmail.com>
2026-09-26 08:13:27 +0300

    S

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    barcode: { type: String, unique: true, sparse: true },
     type: { type: String, default: 'others' },
     price: { type: Number, default: 0 },
     buying_price: { type: Number, default: 0 },
